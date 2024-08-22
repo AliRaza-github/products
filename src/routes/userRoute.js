6df0c625495077f2aef8b527b4499b09cdca7228
Ali Raza <123078445+AliRaza-github@users.noreply.github.com>
2024-08-22 16:13:13 +0500

    remove validater

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { registerValidator, loginValidator } = require("../validater/validater")
 const { register, login } = require("../controllers/userController")
 
-router.post('/register', registerValidator, register);
+router.post('/register', register);
 router.post('/login', loginValidator, login);
 
 module.exports = router;

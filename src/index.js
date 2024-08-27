463f3c3ec4cd9b3f84c6b1b683224f26d49ca5c2
Ali Raza <123078445+AliRaza-github@users.noreply.github.com>
2024-08-27 11:12:01 +0500

    remove serverless issue

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// const serverless = require('serverless-http');
+const serverless = require('serverless-http');
 const mongoose = require("mongoose");
 const express = require("express");
 const cors = require('cors');

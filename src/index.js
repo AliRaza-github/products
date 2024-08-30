e3a98b7917f7c8011007227add0b9eb842257d4a
Ali Raza <123078445+AliRaza-github@users.noreply.github.com>
2024-08-30 17:47:51 +0500

    init -y

--- src/index.js
+++ src/index.js
@@ -1,7 +1,52 @@
-// const serverless = require('serverless-http');
+// // const serverless = require('serverless-http');
+// const mongoose = require("mongoose");
+// const express = require("express");
+// const cors = require('cors');
+// const authRoutes = require("./routes/authRoute");
+// const dashboardTabs = require("./routes/dashboardTabsRoute");
+// const chatRoutes = require('./routes/chatRoute');
+// const notification = require("./routes/notificationRoute");
+// const admin = require("./routes/adminRoutes");
+// const bookTour = require("./routes/bookingTourRoute");
+
+// require("dotenv").config();
+// const mongoUri = process.env.MONGO_URI;
+
+// const app = express();
+// app.use(cors({
+//   origin: '*',
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// }));
+// app.use(express.json());
+
+// app.use("/api/auth", authRoutes);
+// app.use("/api/dashboard", dashboardTabs);
+// app.use("/api", notification);
+// app.use("/api/admin", admin);
+// app.use('/api/chat', chatRoutes);
+// app.use('/api/tour', bookTour);
+
+// app.get('/home', (req, res) => {
+//   res.status(200).send('home vercel');
+// });
+// //
+// mongoose.connect(mongoUri).then(() => {
+//   console.log("DB is connected")
+//   app.listen(3000, () => {
+//     console.log("app is Running at port 3000 ");
+//   })
+// }).catch((error) => {
+//   console.log("DB connection Error..", error);
+// })
+
+
 const mongoose = require("mongoose");
 const express = require("express");
 const cors = require('cors');
+const http = require('http');
+const socketIo = require('socket.io');
+
 const authRoutes = require("./routes/authRoute");
 const dashboardTabs = require("./routes/dashboardTabsRoute");
 const chatRoutes = require('./routes/chatRoute');
@@ -13,8 +58,17 @@ require("dotenv").config();
 const mongoUri = process.env.MONGO_URI;
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: 'http://localhost:3001', // React app URL
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }
+});
+
 app.use(cors({
-  origin: '*',
+  origin: 'http://localhost:3001', // React app URL
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
@@ -30,12 +84,26 @@ app.use('/api/tour', bookTour);
 app.get('/home', (req, res) => {
   res.status(200).send('home vercel');
 });
-//
+
+// Set up Socket.IO connection
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('message', (msg) => {
+    console.log('Message received:', msg);
+    io.emit('message', msg); // Broadcast message to all clients
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
 mongoose.connect(mongoUri).then(() => {
-  console.log("DB is connected")
-  app.listen(3000, () => {
-    console.log("app is Running at port 3000 ");
-  })
+  console.log("DB is connected");
+  server.listen(3000, () => {
+    console.log("App is running at port 3000");
+  });
 }).catch((error) => {
-  console.log("DB connection Error..", error);
-})
+  console.log("DB connection Error:", error);
+});
